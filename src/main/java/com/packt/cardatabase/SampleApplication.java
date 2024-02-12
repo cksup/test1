6da501b5b2a675a3f676502f3835aa064d8bbc72
cks <cksup@naver.com>
2024-02-12 19:51:18 +0900

    2nd

--- src/main/java/com/packt/cardatabase/SampleApplication.java
+++ src/main/java/com/packt/cardatabase/SampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleApplication {
-
+//이것은 주석입니다
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
 	}
